83e7e8fbbe115ace7504dd425bbc4a40391b3d37
Sridhar v <vsri02498@gmail.com>
2026-03-23 07:00:28 +0530

    Add Firebase initialization and configuration code

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+// Initialize Firebase
+const firebase = require('firebase/app');
+firebase.initializeApp(firebaseConfig);
+
+module.exports = firebase;
